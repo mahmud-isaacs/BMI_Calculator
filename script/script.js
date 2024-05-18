1b1f711546faf6774af0d05c33262607e516b820
Mahmud.isaacs03 <Mahmud.isaacs03@gmail.com>
2024-05-18 13:43:19 +0200

    final

--- script/script.js
+++ script/script.js
@@ -1,4 +1,38 @@
-let age = document.querySelector('#age')
+let btn = document.getElementById("calculate");
 
-let height = document.querySelector('#height')
-let weight = document.querySelector('#weight')
+btn.addEventListener("click", function () {
+  let heightCm = document.querySelector("#height").value;
+  let weight = document.querySelector("#weight").value;
+
+  if (heightCm == "" || weight == "") {
+    alert("Please fill out the input fields!");
+    return;
+  }
+ 
+  let heightM = heightCm / (100) * 2;
+
+  let BMI = weight / heightM ;
+
+  BMI = BMI.toFixed(2)
+
+  document.querySelector('#result').innerHTML = BMI
+
+  let status = "";
+
+  if (BMI < 18.5){
+    status = 'Underweight'
+  }
+  if (BMI >= 18.5 && BMI < 25){
+    status = 'Normal Weight'
+  }
+  if (BMI >= 25 && BMI < 30){
+    status = 'Overweight'
+  }
+  if (BMI >= 30){
+    status = 'Obese'
+  }
+
+  document.querySelector('.comment').innerHTML = `You are   <span id='comment'>${status}</span>`;
+});
+
+
